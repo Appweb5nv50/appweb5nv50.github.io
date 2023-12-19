93430364825a72335fd77b868deac7ca6b578777
Appweb5nv50 <154292843+Appweb5nv50@users.noreply.github.com>
2023-12-18 20:25:28 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,10 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+  apiKey: "AIzaSyA6AAG0FFMEiIuvIcsXkfuNoK49THoPh6k",
+  authDomain: "aplicacion-web-1567f.firebaseapp.com",
+  projectId: "aplicacion-web-1567f",
+  storageBucket: "aplicacion-web-1567f.appspot.com",
+  messagingSenderId: "1083920231627",
+  appId: "1:1083920231627:web:b8da723a92d0b832ae7f4e
 });
