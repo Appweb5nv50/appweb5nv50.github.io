edac223e7eba341ca2f167776214de730501d589
Appweb5nv50 <154292843+Appweb5nv50@users.noreply.github.com>
2023-12-18 20:31:29 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,10 +8,10 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyA6AAG0FFMEiIuvIcsXkfuNoK49THoPh6k",
+ apiKey: "AIzaSyA6AAG0FFMEiIuvIcsXkfuNoK49THoPh6k",
   authDomain: "aplicacion-web-1567f.firebaseapp.com",
   projectId: "aplicacion-web-1567f",
   storageBucket: "aplicacion-web-1567f.appspot.com",
   messagingSenderId: "1083920231627",
-  appId: "1:1083920231627:web:b8da723a92d0b832ae7f4e
+  appId: "1:1083920231627:web:b8da723a92d0b832ae7f4e"
 });
